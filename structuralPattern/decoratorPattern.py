d41492697aeeabfd74ae1d6fa21beb5391025f61
Hammad-001 <hammadfaisal178@gmail.com>
2023-02-19 18:10:02 +0500

    decorator pattern added

--- structuralPattern/decoratorPattern.py
+++ structuralPattern/decoratorPattern.py
@@ -1,54 +1,113 @@
-from abc import ABC, abstractclassmethod
+from __future__ import annotations
+from abc import ABC, abstractmethod
 
 
-class Notifications(ABC):
-    @abstractclassmethod
-    def notify(self):
-        pass
+class Notifier(ABC):
+
+    @abstractmethod
+    def notify(self, notification: str):
+        """Notify about notification."""
+        return notification
 
 
-class UserNotifications(Notifications):
-    def notify(self):
-        print("Email Notified!")
+class EmailNotifier(Notifier):
+
+    def notify(self, notification: str):
+        """Notify on Email about notification."""
+        return f"Email: {notification}"
 
 
-class UserNotificationDecorator(Notifications):
-    source = None
+class SMSNotifier(Notifier):
 
-    def __init__(self, source):
-        self.source = source
-
-    def notify(self):
-        self.source.notify()
+    def notify(self, notification: str):
+        """Notify on SMS about notification."""
+        return f"SMS: {notification}"
 
 
-class ExtraNotification(UserNotificationDecorator):
-    def notify(self, notify):
-        for i in notify:
-            val = i.lower()
-            if val == 'facebook':
-                self.facebookNotify()
-            if val == 'instagram':
-                self.instagramNotify()
-            if val == 'twitter':
-                self.twitterNotify()
+class FacebookNotifier(Notifier):
 
-    def facebookNotify(self):
-        print('Facebook Notified!')
-
-    def instagramNotify(self):
-        print('Instagram Notified!')
-
-    def twitterNotify(self):
-        print('Twitter Notified!')
+    def notify(self, notification: str):
+        """Notify on Facebook about notification."""
+        return f"Facebook: {notification}"
 
 
-# Simple User Notifications
-userNotifications = UserNotifications()
-userNotifications.notify()
+class SlackNotifier(Notifier):
 
-# Extended User Notifications Using Decorators
-extraNotification = ExtraNotification(userNotifications)
-extraNotification.notify(['facebook'])
-# extraNotification.notify(['facebook', 'instagram'])
-# extraNotification.notify(['facebook', 'instagram', 'Twitter'])
+    def notify(self, notification: str):
+        """Notify on Slack about notification."""
+        return f"Slack: {notification}"
+
+
+class NotifierDecorator(Notifier):
+
+    def __init__(self, notifier: Notifier = None):
+        self.__notifier = notifier
+
+    def get_notifier(self):
+        """Get the current notifier"""
+        return self.__notifier
+
+    def notify(self, notification: str):
+        """Notify via given notifier"""
+        return self.__notifier.notify(notification)
+
+
+class EmailNotifierDecorator(NotifierDecorator):
+
+    def notify(self, notification: str):
+        """Decorated Email Notifier"""
+        return f"Email: {self.get_notifier().notify(notification)}"
+
+
+class SMSNotifierDecorator(NotifierDecorator):
+
+    def notify(self, notification: str):
+        """Decorated SMS Notifier"""
+        return f"SMS: {self.get_notifier().notify(notification)}"
+
+
+class FacebookNotifierDecorator(NotifierDecorator):
+
+    def notify(self, notification: str):
+        """Decorated Facebook Notifier"""
+        return f"Facebook: {self.get_notifier().notify(notification)}"
+
+
+class SlackNotifierDecorator(NotifierDecorator):
+
+    def notify(self, notification: str):
+        """Decorated Slack Notifier"""
+        return f"Slack: {self.get_notifier().notify(notification)}"
+
+
+if __name__ == "__main__":
+
+    # Simple Notifiers
+    email_notifier = EmailNotifier()
+    print(email_notifier.notify("Notified!"))
+
+    sms_notifier = SMSNotifier()
+    print(sms_notifier.notify("Notified!"))
+
+    facebook_notifier = FacebookNotifier()
+    print(facebook_notifier.notify("Notified!"))
+
+    slack_notifier = SlackNotifier()
+    print(slack_notifier.notify("Notified!"))
+
+    # Decorated Notifiers
+    sms_and_slack_notifier = SlackNotifierDecorator(sms_notifier)
+    print(sms_and_slack_notifier.notify("Notified!"))
+
+    sms_and_facebook_notifier = FacebookNotifierDecorator(sms_notifier)
+    print(sms_and_facebook_notifier.notify("Notified!"))
+
+    facebook_and_slack_notifier = FacebookNotifierDecorator(slack_notifier)
+    print(facebook_and_slack_notifier.notify("Notified!"))
+
+    facebook_sms_slack_notifier = FacebookNotifierDecorator(
+        sms_and_slack_notifier)
+    print(facebook_sms_slack_notifier.notify("Notified!"))
+
+    # Furthermore, Image is added in images folder,
+    # for pictorial view of current Example
